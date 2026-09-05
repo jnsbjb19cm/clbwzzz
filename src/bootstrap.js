e22950cfa17aafba318047ad3d4bb5a9b3591aef
jnsbjb19cm <tianqiag_el@163.com>
2026-09-05 12:14:58 +0800

    fix: install craft binding refund safety

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,8 +1,10 @@
 import { installEconomyInventoryRules20260905 } from './ui/EconomyInventoryRules20260905.js';
 import { installEconomyInventoryPersistence20260905 } from './ui/EconomyInventoryPersistence20260905.js';
+import { installCraftBindingSafety20260905 } from './ui/CraftBindingSafety20260905.js';
 
 // 必须在 App 创建 InventoryStore / LoginView / SmithyView 实例之前安装。
 installEconomyInventoryRules20260905();
 installEconomyInventoryPersistence20260905();
+installCraftBindingSafety20260905();
 
 void import('./main.js');
