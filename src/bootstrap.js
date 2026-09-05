4e374aebd9e65ce52b09d51fbbbefd142c9a2a12
jnsbjb19cm <tianqiag_el@163.com>
2026-09-05 12:11:12 +0800

    feat: install economy rules before app startup

---
+++ src/bootstrap.js
@@ -0,0 +1,8 @@
+import { installEconomyInventoryRules20260905 } from './ui/EconomyInventoryRules20260905.js';
+import { installEconomyInventoryPersistence20260905 } from './ui/EconomyInventoryPersistence20260905.js';
+
+// 必须在 App 创建 InventoryStore / LoginView / SmithyView 实例之前安装。
+installEconomyInventoryRules20260905();
+installEconomyInventoryPersistence20260905();
+
+await import('./main.js');
